65b96363a3470b9344ee3a84ae8fcf54a0e4f37a
Josh <joshua.ghattas@gmail.com>
2025-09-06 08:00:07 -0500

    Fixed BareMux URL

--- ui/bare-mux-connection.tsx
+++ ui/bare-mux-connection.tsx
@@ -11,7 +11,7 @@ export default function BareMuxConnectionComponent() {
       await connection.setTransport("/epoxy/index.mjs", [
         { wisp: "wss://wisp.mercurywork.shop/" },
       ]);
-      await connection.setTransport("/baremod/index.mjs", ["https://tomp.app/"]);
+      await connection.setTransport("/baremux/index.mjs", ["https://tomp.app/"]);
     }
 
     initConnection().catch(console.error);
