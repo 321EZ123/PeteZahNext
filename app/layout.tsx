961700faee6b1ee64cdce46ed84a1d88385fae28
Josh <joshua.ghattas@gmail.com>
2025-09-07 18:28:42 -0500

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,22 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-SHE360M0YP"
         />
+        <Script>
+          {`(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({
+                "gtm.start": new Date().getTime(),
+                event: "gtm.js",
+              });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-WPH7NCG4");
+            `}
+        </Script>
         <Script id="google-analytics" strategy="afterInteractive">
           {/* this just makes sure that Next doesn't check the code (trust me, it works) */}
           {`
@@ -62,6 +78,14 @@ export default function RootLayout({
         />
       </head>
       <body className="w-full h-full min-h-screen">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WPH7NCG4"
+            height="0"
+            width="0"
+            style={{display: "none", visibility: "hidden"}}
+          ></iframe>
+        </noscript>
         <GooglebotVerifier />
         {children}
       </body>
